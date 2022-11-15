2e5a69d7bae148b0aded0308fab39b8155ecc957
JakobDev <jakobdev@gmx.de>
2022-11-15 13:51:28 +0000

    Add Windows Installer

--- deploy/sourceforge.py
+++ deploy/sourceforge.py
@@ -1,47 +1,60 @@
 #!/usr/bin/env python3
-import subprocess
+import tempfile
+import requests
+import pathlib
 import shutil
 import pysftp
 import os
 
-rootDir = os.path.dirname(os.path.realpath(__file__))[:-7]
+
+SOURCEFORGE_PROJECT = "jdminecraftlauncher"
+APPNAME = "jdMinecraftLauncher"
+
+root_dir = str(pathlib.Path(__file__).parent.parent)
 version = os.getenv("CI_COMMIT_TAG")
 
-os.mkdir(os.path.join(rootDir,"work"))
-os.mkdir(os.path.join(rootDir,"output"))
 
-workdir = os.path.join(rootDir,"work")
+def create_new_dir(path: str):
+    try:
+        shutil.rmtree(path)
+    except Exception:
+        pass
+    os.makedirs(path)
 
-shutil.copytree(os.path.join(rootDir,"jdMinecraftLauncher"),os.path.join(workdir,"jdMinecraftLauncher"))
-shutil.copyfile(os.path.join(rootDir,"jdMinecraftLauncher.py"),os.path.join(workdir,"jdMinecraftLauncher.py"))
-shutil.copyfile(os.path.join(rootDir,"README.md"),os.path.join(workdir,"README.md"))
-shutil.copyfile(os.path.join(rootDir,"LICENSE"),os.path.join(workdir,"LICENCE"))
-shutil.copyfile(os.path.join(rootDir,"requirements.txt"),os.path.join(workdir,"requirements.txt"))
 
-zipOutput = os.path.join(rootDir,"output","jdMinecraftLauncher-" + version + "-Python")
-shutil.make_archive(zipOutput,'zip',workdir)
+def create_source_zip():
+     with tempfile.TemporaryDirectory() as workdir:
+        shutil.copytree(os.path.join(root_dir, APPNAME), os.path.join(workdir, APPNAME))
+        shutil.copyfile(os.path.join(root_dir,f"{APPNAME}.py"),os.path.join(workdir, f"{APPNAME}.py"))
+        shutil.copyfile(os.path.join(root_dir, "README.md"),os.path.join(workdir, "README.md"))
+        shutil.copyfile(os.path.join(root_dir, "LICENSE"),os.path.join(workdir, "LICENCE"))
+        shutil.copyfile(os.path.join(root_dir, "requirements.txt"),os.path.join(workdir, "requirements.txt"))
 
-zipOutput = zipOutput + ".zip"
+        shutil.make_archive(os.path.join(root_dir, "output", f"{APPNAME}-{version}-Python"), "zip", workdir)
 
-#Create windows zip
-exe_dir_name = os.listdir(os.path.join(rootDir,"build"))[0]
-exe_dir_path = os.path.join(rootDir,"build",exe_dir_name)
 
-windowsOutput = os.path.join(rootDir,"output","jdMinecraftLauncher-" + version + "-Windows")
-shutil.make_archive(windowsOutput,'zip',exe_dir_path)
+def upload_files():
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+    server = pysftp.Connection(host="frs.sourceforge.net", username=os.getenv("SOURCEFORGE_USERNAME"), password=os.getenv("SOURCEFORGE_PASSWORD"), cnopts=cnopts)
+    server.chdir(f"/home/frs/project/{SOURCEFORGE_PROJECT}")
+    server.mkdir(version)
+    server.chdir(f"/home/frs/project/{SOURCEFORGE_PROJECT}/{version}")
+    for i in os.listdir(os.path.join(root_dir, "output")):
+        server.put(os.path.join(root_dir, "output", i))
+    server.close()
 
-windowsOutput = windowsOutput + ".zip"
+    requests.put(f"https://sourceforge.net/projects/{SOURCEFORGE_PROJECT}/files/{version}/{APPNAME}-{version}-Python.zip", {"default": "mac&default=linux&default=android&default=bsd&default=solaris&default=others", "api_key": os.getenv("SOURCEFORGE_API_KEY")},  headers={"Accept": "application/json"})
+    requests.put(f"https://sourceforge.net/projects/{SOURCEFORGE_PROJECT}/files/{version}/{APPNAME}-{version}-WindowsInstaller.exe", {"default": "windows", "api_key": os.getenv("SOURCEFORGE_API_KEY")},  headers={"Accept": "application/json"})
 
-#Upload files
-cnopts = pysftp.CnOpts()
-cnopts.hostkeys = None
-server = pysftp.Connection(host="frs.sourceforge.net",username=os.getenv("SOURCEFORGE_USERNAME"),password=os.getenv("SOURCEFORGE_PASSWORD"),cnopts=cnopts)
-server.chdir("/home/frs/project/jdminecraftlauncher")
-server.mkdir(version)
-server.chdir("/home/frs/project/jdminecraftlauncher/" + version)
-server.put(zipOutput)
-server.put(windowsOutput)
-server.close()
 
-subprocess.call(["curl","-H","Accept: application/json","-X","PUT","-d","default=mac&default=linux&default=bsd&default=solaris&default=others","-d","api_key=" + os.getenv("SOURCEFORGE_API_KEY"),"https://sourceforge.net/projects/jdminecraftlauncher/files/" + version + "/jdMinecraftLauncher-" + version + "-Python.zip"])
-subprocess.call(["curl","-H","Accept: application/json","-X","PUT","-d","default=windows","-d","api_key=" + os.getenv("SOURCEFORGE_API_KEY"),"https://sourceforge.net/projects/jdminecraftlauncher/files/" + version + "/jdMinecraftLauncher-" + version + "-Windows.zip"])
+def main():
+    create_new_dir(os.path.join(root_dir, "output"))
+    create_source_zip()
+    shutil.make_archive(os.path.join(root_dir, "output", f"{APPNAME}-{version}-WindowsPortable"), "zip", os.path.join(root_dir, "WindowsBuild", "Portable"))
+    shutil.copyfile(os.path.join(root_dir, "WindowsBuild", f"{APPNAME}Installer.exe"), os.path.join(root_dir, "output", f"{APPNAME}-{version}-WindowsInstaller.exe"))
+    upload_files()
+
+
+if __name__ == "__main__":
+    main()
