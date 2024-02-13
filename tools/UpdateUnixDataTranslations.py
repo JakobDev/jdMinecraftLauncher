887cc4b0061d6118fbbd360af32f559043521be5
JakobDev <jakobdev@gmx.de>
2024-02-13 18:45:06 +0100

    Add offline mode to desktop file

--- tools/UpdateUnixDataTranslations.py
+++ tools/UpdateUnixDataTranslations.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 import subprocess
 import pathlib
+import sys
+
+
+try:
+    import polib
+except ModuleNotFoundError:
+    print("polib not installed. Please install it with pip.", file=sys.stderr)
+    sys.exit(1)
+
+
+def filter_name(path: pathlib.Path) -> None:
+    pofile = polib.pofile(path)
+
+    for pos, msg in enumerate(pofile):
+        if msg.msgid == "jdMinecraftLauncher":
+            del pofile[pos]
+            break
+
+    pofile.save(path)
 
 
 def main() -> None:
@@ -8,7 +27,9 @@ def main() -> None:
     pot_path = str(root_path / "deploy" / "translations" / "messages.pot")
 
     subprocess.run(["xgettext", "-l", "xml", "--its", str(root_path / "deploy" / "translations" / "AppStream.its"), "-o", pot_path, "deploy/page.codeberg.JakobDev.jdMinecraftLauncher.metainfo.xml"], cwd=root_path, check=True)
-    subprocess.run(["xgettext", "-l", "desktop", "-k", "-kComment", "-o", pot_path, "-j", "deploy/page.codeberg.JakobDev.jdMinecraftLauncher.desktop"], cwd=root_path, check=True)
+    subprocess.run(["xgettext", "-l", "desktop", "-k", "-kComment", "-kName", "-o", pot_path, "-j", "deploy/page.codeberg.JakobDev.jdMinecraftLauncher.desktop"], cwd=root_path, check=True)
+
+    filter_name(pot_path)
 
     for file in (root_path / "deploy" / "translations").iterdir():
         if file.suffix == ".po":
